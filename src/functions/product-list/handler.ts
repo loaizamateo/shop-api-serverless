e2d44937af2c045ed8973fe39199700b2fbc003a
loaizamateo <mateo_loaiza@epam.com>
2022-07-12 15:17:40 -0500

    import list of products from json file

--- src/functions/product-list/handler.ts
+++ src/functions/product-list/handler.ts
@@ -1,28 +1,13 @@
 // import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
-// import products from '../product-list/mock.json' assert {type: 'json'};
+import { default as products } from '../product-list/productList.json';
 
 // import schema from './schema';
 
 // const getProductList: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async () => {
   const getProductList = async () => {
-  return formatJSONResponse([
-    {
-      "count": 3,
-      "description": "Short Product Description1",
-      "id": "7567ec4b-b13c-48c5-9d45-fc73c48a80aa",
-      "price": 2.2,
-      "title": "ProductUpdated"
-    },
-    {
-      "count": 2,
-      "description": "Short Product Description2",
-      "id": "7563ec4b-b13c-48c5-9d45-fc71c48a80aa",
-      "price": 2,
-      "title": "ProductUpdated2"
-    }
-  ]);
+  return formatJSONResponse(products);
 };
 
 export const main = middyfy(getProductList);
