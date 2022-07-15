93f85639bd29ba0d2e937da0b8f485e463a361e5
loaizamateo <mateo_loaiza@epam.com>
2022-07-15 14:50:29 -0500

    - CORS module added

--- src/functions/products/handler.ts
+++ src/functions/products/handler.ts
@@ -2,13 +2,14 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 import { default as products } from '../../utils/productList.json';
+import cors from '@middy/http-cors';
 
 export const getProductsList = middyfy(async (): Promise<APIGatewayProxyResult> => {
   return formatJSONResponse(products);
-});
+}).use(cors());
 
 export const getProductsById = middyfy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const id = event.pathParameters.id;
   const product = products.find(p => p.id === id);
   return formatJSONResponse(product);
-});
+}).use(cors());
